150f1071e81f25ef8a8e2e2e92865f4a0c6119fe
DineshDinu1998 <47448233+DineshDinu1998@users.noreply.github.com>
2019-02-08 16:48:48 +0530

    Create asaami.c

---
+++ asaami.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=2;
+	printf("Enter the number %d",a);
+	if(a%2==0)
+	{
+		printf("\nThe entered no is even ");
+	
+	}
+	else
+	{
+		printf("\nThe entered no is odd");
+	}
+	return 0;
+}

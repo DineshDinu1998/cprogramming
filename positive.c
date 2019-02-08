937e46c4eee0778bed275b8edb64efcf1ca90c41
DineshDinu1998 <47448233+DineshDinu1998@users.noreply.github.com>
2019-02-08 19:48:57 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("enter the no %d",a);
+	if(a>0)
+	{
+	printf("\npositive");
+}
+else if(a=0)
+{
+	printf("\nzero");
+	
+}
+	else
+	{
+		printf("\nnegative");
+	}
+	return 0;
+}

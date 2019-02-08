7eaff6da3d77293dc3b54e85d2c4eaac1d06b91f
DineshDinu1998 <47448233+DineshDinu1998@users.noreply.github.com>
2019-02-08 19:00:57 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+char c;
+	scanf(" %c",&c);
+	
+	if(c== 'a' || c=='b' || c=='c' || c=='d' || c=='e' || c== 'f' || c=='g' || c=='h' || c=='i' || c=='j'||c== 'k' || c=='l' || c=='m' || c=='n' || c=='o' || c== 'p' || c=='q' || c=='r' || c=='s' || c=='t' ||c== 'U' || c=='V' || c=='W' || c=='X' || c=='Y'||c== 'Z')
+	{
+		printf("%c ia a ALP",c);
+		
+	}
+	else
+	{
+		printf("\n not a alp");
+	}
+	return 0;
+}
